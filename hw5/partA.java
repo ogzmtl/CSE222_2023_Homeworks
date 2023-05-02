45c0ceba36807f07cc84978b0605890f9cfc9d8d
oguz <oguzmutlu81@gmail.com>
2023-05-02 12:15:28 +0300

    part1 done

--- hw5/partA.java
+++ hw5/partA.java
@@ -1,13 +1,15 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import javax.swing.*;  
+import javax.swing.tree.DefaultMutableTreeNode;  
 
 
 
 public class partA{
     
-    private String[][] txtToArray = new String[5][];
-    
+    private String[][] txtToArray = new String[1][];
+
     public void readFromTxt(String filename) throws FileNotFoundException{
         File file = new File(filename);
         String line;
@@ -28,25 +30,23 @@ public class partA{
                 for(int i = 0; i < splitted.length; i++){
                     txtToArray[counter][i] = splitted[i];
                 }
-                if(counter == txtToArray.length-1){
-                    txtToArray = reallocate(counter);
+                if(counter == txtToArray.length-1 && (scanner.hasNextLine())){
+                    txtToArray = reallocate(counter+1);
                 }  
                 counter++;           
             }
-            for(int i = 0; i < txtToArray.length; i++)
-            {
-                for(int j = 0; j < txtToArray[i].length; j++){
-                    System.out.print(txtToArray[i][j]);
-                }
-                System.out.print("\n");
-            }
+            // for(int i = 0; i < txtToArray.length; i++)
+            // {
+            //     for(int j = 0; j < txtToArray[i].length; j++){
+            //         System.out.print(txtToArray[i][j] + " ");
+            //     }
+            //     System.out.print("\n");
+            // }
         }
-        
-
     }
 
     private String[][] reallocate(int counter){
-        String[][] tempString = new String[5+counter][];
+        String[][] tempString = new String[1+counter][];
 
         for(int i = 0; i < txtToArray.length; i++)
         {
@@ -60,4 +60,93 @@ public class partA{
         }
         return tempString;
     }
+
+    public void tree(){
+        DefaultMutableTreeNode tree = new DefaultMutableTreeNode("root");
+        DefaultMutableTreeNode temp = tree;
+
+        JFrame fa;  
+
+        fa = new JFrame();
+        for(int i = 0; i < txtToArray.length; i++){
+            // DefaultMutableTreeNode year = new DefaultMutableTreeNode(txtToArray[i][0]);
+            // tree.add(year);
+            // temp = traverseAllTree(tree);
+            // System.out.println(tree.getUserObject());
+            // insertToTree(tree, year);
+            temp = tree;
+            for(int j = 0; j < txtToArray[i].length; j++){
+
+                DefaultMutableTreeNode lecture = new DefaultMutableTreeNode(txtToArray[i][j]);
+                // System.out.println(year.getUserObject());
+                // System.out.println(lecture.getUserObject());
+                // insertToTree(year, lecture);
+
+                if(insertToTree(temp, lecture)){
+                    temp.add(lecture);
+                    temp = lecture;
+                }
+                else{
+                    temp = moveTreeObject(temp, lecture);
+                }   
+            }
+            System.out.println("------------");
+        }
+        // insertToTree(tree, null);
+        JTree jt=new JTree(tree);  
+        fa.add(jt);  
+        fa.setSize(200,200);  
+        fa.setVisible(true);  
+    }
+
+    public boolean insertToTree(DefaultMutableTreeNode tree,DefaultMutableTreeNode node){
+        if(tree.isLeaf()){
+        System.out.println("LeafinsertToTree " + tree.getUserObject() + " node: " + node.getUserObject()); 
+            return true;
+        }
+        else{
+            if(!isInclude(tree, node)){
+                System.out.println("insertToTree " + tree.getUserObject() + " node: " + node.getUserObject()); 
+                return true;
+            }
+
+        }
+        System.out.println("false");
+        return false;
+    }
+    
+    private boolean isInclude(DefaultMutableTreeNode tree,DefaultMutableTreeNode node){
+        if (tree == null || node == null) {
+            return false;
+        }   
+        int childCount = tree.getChildCount();
+        System.out.println("tree : " + tree.getUserObject() +" "+childCount);
+        for(int i=0;i<childCount;i++){
+     
+            DefaultMutableTreeNode child = (DefaultMutableTreeNode) tree.getChildAt(i);
+            if(child == null) {
+                return true;
+            }
+            if(node.getUserObject().equals(child.getUserObject())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private DefaultMutableTreeNode moveTreeObject(DefaultMutableTreeNode tree,DefaultMutableTreeNode node){
+
+        int childCount = tree.getChildCount();
+        System.out.println("tree : " + tree.getUserObject() +" "+childCount);
+        for(int i=0;i<childCount;i++){
+     
+            DefaultMutableTreeNode child = (DefaultMutableTreeNode) tree.getChildAt(i);
+
+            if(node.getUserObject().equals(child.getUserObject())){
+                return child;
+            }
+        }
+        return null;
+    }
 }
+
--- hw5/testMain.java
+++ hw5/testMain.java
@@ -8,5 +8,6 @@ public class testMain {
 
 
         first.readFromTxt(filename);
+        first.tree();
     }
 }
